b02961255f19edef84eb129dd6b93cd3c67756cb
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-04-23 15:39:22 -0300

    ingreso clave terminado

---
+++ Assets/Scripts/ClaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMansage;
+
+    string clave = "tequierogero";
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMansage.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ComprobarClave()
+    {
+        string claveIngresada = inputClave.text;
+        if (claveIngresada!=clave)
+        {
+            txtMansage.text = "Acceso denegado";
+            inputClave.text = "";
+        }
+        else
+        {
+            txtMansage.text = "Bienvenido";
+        }
+    }
+}
